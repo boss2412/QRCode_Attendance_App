27fcc33c4fa323b5ccef65bb8ce126f0e1b8f0a5
ANURAG TIWARI <tiwarianurag241204@gmail.com>
2024-03-08 15:45:42 +0530

    Anurag Commit 6

--- main.py
+++ main.py
@@ -1,60 +1,47 @@
-from kivy.app import App
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+
+from Student.student_login import StudentLoginRegister
+from Teacher.register_screen import RegisterScreen
+from Student.student_register import StudentRegisterScreen
+from Teacher.teacher_login_register import TeacherLoginRegister
+from welcome_screen import WelcomeScreen
 from kivy.core.window import Window
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button
-from Student.student1 import Login1Screen, Register1Screen
-from Teacher.teacher1 import Login2Screen, Register2Screen
+from kivy.core.text import LabelBase
+
+#To get output that would appear on mobile
+Window.size = (360, 640)
+
+#For Custom Font Style
+LabelBase.register(name="MPoppins", fn_regular="C:/Users/anura/PycharmProjects/MiniProject/fonts/Poppins/Poppins"
+                                               "-Medium.ttf",
+                   )
+
+LabelBase.register(name="BPoppins", fn_regular="C:/Users/anura/PycharmProjects/MiniProject/fonts/Poppins/Poppins"
+                                               "-SemiBold.ttf",
+                   )
 
 
-Window.clearcolor = 1, 1, 1, 1
-
-
-def switch_to_teacher1(instance):
-    app = App.get_running_app()
-    app.root.current = 'login2'
-
-
-def switch_to_student1(instance):
-    app = App.get_running_app()
-    app.root.current = 'login1'
-
-
-class MainScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-        self.button1 = Button(
-            size_hint=(1, 1),
-            background_normal='student1.png',
-            background_down='student2.png'
-        )
-        self.button1.bind(on_release=switch_to_student1)
-
-        layout.add_widget(self.button1)
-
-        self.button2 = Button(
-            size_hint=(1, 1),
-            background_normal='teacher1.png',
-            background_down='teacher2.png'
-        )
-        self.button2.bind(on_release=switch_to_teacher1)
-
-        layout.add_widget(self.button2)
-
-        self.add_widget(layout)
-
-
-class MyApp(App):
+class MainApp(MDApp):
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(MainScreen(name='main'))
-        sm.add_widget(Login1Screen(name='login1'))
-        sm.add_widget(Register1Screen(name='register1'))
-        sm.add_widget(Login2Screen(name='login2'))
-        sm.add_widget(Register2Screen(name='register2'))
-        return sm
+        # Load the KV files
+        Builder.load_file('welcome_screen.kv')
+        Builder.load_file('Student/student_login.kv')
+        Builder.load_file('Teacher/teacher_login_register.kv')
+        Builder.load_file('Teacher/register_screen.kv')
+        Builder.load_file('Student/student_register.kv')
+
+
+        # Initialize the ScreenManager
+        self.sm = ScreenManager()
+        self.sm.add_widget(WelcomeScreen(name='welcome'))
+        self.sm.add_widget(StudentLoginRegister(name='student_login_register'))
+        self.sm.add_widget(TeacherLoginRegister(name='teacher_login_register'))
+        self.sm.add_widget(RegisterScreen(name='register_screen'))
+        self.sm.add_widget(StudentRegisterScreen(name='student_register_screen'))
+        return self.sm
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    MainApp().run()
